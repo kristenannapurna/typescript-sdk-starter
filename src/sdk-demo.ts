25b9053dbaf09f2dd776bc388e701955b49e4d93
Kristen Spencer <kristenamspencer@gmail.com>
2019-12-19 16:29:31 -0500

    trying to trigger travis

--- src/sdk-demo.ts
+++ src/sdk-demo.ts
@@ -1,4 +1,6 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
 // import "core-js/fn/array.find"
 // ...
-export const CoolSDK = {}
+
+const someMethod = () => true
+export const CoolSDK = { someMethod: someMethod }
